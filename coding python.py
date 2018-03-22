cc7b056cd1a8480ab1aa027c81b761e4c5eb8397
Tiararahayuruhyat17 <37565106+Tiararahayuruhyat17@users.noreply.github.com>
2018-03-22 14:47:58 +0700

    Add files via upload

---
+++ coding python.py
@@ -0,0 +1,7 @@
+nama=raw_input("masukan nama bidang :")
+panjang=raw_input("panjang :")
+lebar=raw_input("lebar :")
+print "Bidang tersebut adalah ",nama
+print "luas adalah"
+Luas=panjang*lebar
+print(luas)
